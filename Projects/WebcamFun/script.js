57adcda24bc4024211cc646a51b3c085def4785d
Akhand Mishra <akkimysite@gmail.com>
2018-11-07 18:58:37 +0530

    Project Webcam Fun

---
+++ Projects/WebcamFun/script.js
@@ -0,0 +1,97 @@
+const video = document.querySelector('.player');
+const canvas = document.querySelector('.photo');
+const ctx = canvas.getContext('2d');
+const strip = document.querySelector('.strip');
+const snap = document.querySelector('.snap');
+
+
+function getVideo() {
+    navigator.mediaDevices.getUserMedia({ video: true, audio: false })
+        .then(localMediaScreen => {
+            video.src = window.URL.createObjectURL(localMediaScreen);
+            video.play();
+        })
+        .catch(err => console.error("Didn't work ", err));
+}
+
+function paintToCanvas() {
+    const width = video.videoWidth;
+    const height = video.videoHeight;
+    console.log(width, height);
+    canvas.width = width;
+    canvas.height = height;
+
+    setInterval(() => {
+        ctx.drawImage(video, 0, 0, width, height);
+        let pixels = ctx.getImageData(0, 0, width, height);
+        // pixels = redEffect(pixels);
+        // pixels = rgbSplit(pixels);
+        // ctx.globalAlpha = 0.05;
+        pixels = greenScreen(pixels);
+        ctx.putImageData(pixels, 0, 0);
+    }, 16)
+
+}
+
+function redEffect(pixels) {
+    for(let i = 0; i < pixels.data.length; i += 4) {
+        pixels.data[i + 0] += 100; 
+        pixels.data[i + 1] -= 50; 
+        pixels.data[i + 2] *= 0.5; 
+    }
+
+    return pixels;
+}
+
+function rgbSplit(pixels) {
+    for(let i = 0; i < pixels.data.length; i += 4) {
+        pixels.data[i - 550] = pixels.data[i + 0]; 
+        pixels.data[i + 500] = pixels.data[i + 1]; 
+        pixels.data[i + 550] = pixels.data[i + 2]; 
+    }
+
+    return pixels;
+}
+
+function greenScreen(pixels) {
+    const levels = {};
+
+    document.querySelectorAll('.rgb input').forEach((input) => {
+        levels[input.name] = input.value;
+    });
+
+    for(let i = 0; i < pixels.data.length; i += 4) {
+        red = pixels.data[i + 0];
+        green = pixels.data[i + 1];
+        blue = pixels.data[i + 2];
+        alpha = pixels.data[i + 3];
+
+        if(red >= levels.rmin
+            && green >= levels.gmin
+            && blue >= levels.bmin
+            && red <= levels.rmax
+            && green <= levels.gmax
+            && blue <= levels.bmax) {
+                pixels.data[i + 3] = 0;
+            }
+    }
+
+    return pixels;
+}
+
+function takePhoto() {
+    //play the sound
+    snap.currentTime = 0;
+    snap.play();
+
+    const data = canvas.toDataURL('image/jpeg');
+    const link = document.createElement('a');
+    link.href = data;
+    link.setAttribute('download', 'test');
+    link.innerHTML = `<img src="${data}" alt="Yo it's me"/>`;
+    strip.insertBefore(link, strip.firstChild);
+}
+
+getVideo();
+
+video.addEventListener('canplay', paintToCanvas);
